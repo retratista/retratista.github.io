b7bd955abd97639948a27abac1d117b58f75847f
Mohamed Ahmed <mohammed.ahmed.m.228@gmail.com>
2021-07-12 06:42:46 +0200

    deploy

---
+++ precache-manifest.af04143b3f995c91106124001b1bd885.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf02fd21b9806d1d03405f54a556a0be",
+    "url": "/img/brand/logo.png"
+  },
+  {
+    "revision": "aa2133d49a43c24a1df44eff37ec7c6b",
+    "url": "/demo.mp4"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "9b5b1771fac155f538cc",
+    "url": "/js/chunk-4e0b8328.ca50e3cc.js"
+  },
+  {
+    "revision": "3031eab51221d1b0ce06",
+    "url": "/js/chunk-52e4de86.6b7d39d1.js"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "419daa977e068ffc8929",
+    "url": "/js/chunk-vendors.3094edca.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "baaf26334824746d4d3d4694ca32d5f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6759caeebdf8957b3377658b8eb6b540",
+    "url": "/img/brand/Blue-Blue-Logo.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "687a5a96d717034b6f2a1752fb4739c6",
+    "url": "/img/brand/brush.png"
+  },
+  {
+    "revision": "5c3337c6149e76dde54123d9e8d70d39",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "992b25ab804cdf7caebd6559011ba0d7",
+    "url": "/img/brand/brush2.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "1510a4fab3ceb8fa340dca165b4b9a36",
+    "url": "/favicon2.png"
+  },
+  {
+    "revision": "740bdd6c5e6ab61bd0b80b5788f1e376",
+    "url": "/img/brand/brush3.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "f54581ed136a8e9e1d42f8bb79f6085f",
+    "url": "/img/brand/headlines/face-refinement.png"
+  },
+  {
+    "revision": "5c3337c6149e76dde54123d9e8d70d39",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "5154c4e1e0122af90b72736ec53a34cf",
+    "url": "/img/brand/pose.png"
+  },
+  {
+    "revision": "86dd9ee529ca1b35d62b28ba9bd749b8",
+    "url": "/img/brand/headlines/head-poses.png"
+  },
+  {
+    "revision": "e519b5d99923b4ef2cc0",
+    "url": "/js/app.30730a5e.js"
+  },
+  {
+    "revision": "84e8a5c725052d2a132048210bcff84a",
+    "url": "/img/brand/Red-Red-Logo.png"
+  },
+  {
+    "revision": "764dd936d7af4c36216449a4d54d09e3",
+    "url": "/img/brand/headlines/voice-generation.png"
+  },
+  {
+    "revision": "b2b1c40ffb75f2e8b4ee0db051f754d4",
+    "url": "/img/brand/headlines/text-generation.png"
+  },
+  {
+    "revision": "5c3337c6149e76dde54123d9e8d70d39",
+    "url": "/img/brand/retratista-logo.png"
+  },
+  {
+    "revision": "8d62572a483487016d5ebec8135a803c",
+    "url": "/img/brand/retratista3.png"
+  },
+  {
+    "revision": "9c5cef774ded5faa15b81c0a963a2cc9",
+    "url": "/img/brand/retratista.png"
+  },
+  {
+    "revision": "3b1706604c6360dd01709a83c9405999",
+    "url": "/img/brand/White-Blue-Logo.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "22ad9e4505367505106b2e185bfcb366",
+    "url": "/img/results/1.jpeg"
+  },
+  {
+    "revision": "01f7d00b582952f58487c00216d3632a",
+    "url": "/img/brush.png"
+  },
+  {
+    "revision": "32e3f6057d9aec30cd523c740bfaae45",
+    "url": "/img/results/3.jpeg"
+  },
+  {
+    "revision": "21cfd98a285e1732c2c41c8804838e94",
+    "url": "/img/brand/retratista2.png"
+  },
+  {
+    "revision": "8368e70e2cb2860c77b3c00197611b50",
+    "url": "/img/results/2.jpeg"
+  },
+  {
+    "revision": "a5e8c23617e491a11e77f74310b36639",
+    "url": "/img/team/double.jpeg"
+  },
+  {
+    "revision": "23185e251e4841d3737aa7c87e765389",
+    "url": "/img/team/dr-mayada.jpeg"
+  },
+  {
+    "revision": "18ee62f7d65eb040f583e1bdbd833805",
+    "url": "/img/team/ramzy.jpeg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "57133801245b434df10f25da92414990",
+    "url": "/img/team/shawky.jpeg"
+  },
+  {
+    "revision": "2dbd3b470f4acd1ec634516f3341e7db",
+    "url": "/img/team/monda.jpeg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "1f753d402574c16bebdc29c86b0d3556",
+    "url": "/img/sample.png"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "419daa977e068ffc8929",
+    "url": "/css/chunk-vendors.e5e3276b.css"
+  },
+  {
+    "revision": "9b5b1771fac155f538cc",
+    "url": "/css/chunk-4e0b8328.1fead564.css"
+  },
+  {
+    "revision": "e519b5d99923b4ef2cc0",
+    "url": "/css/app.e75ea5c5.css"
+  }
+];
